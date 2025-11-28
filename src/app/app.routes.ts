5505cbfd6ca9616b171399bec8efb833c87285b9
CarolGitonga <carolgitongaofficial@gmail.com>
2025-11-28 16:19:07 +0300

    Add route for create user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,20 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { UsersListComponent } from './features/users/users-list/users-list.component';
 import { UserDetailsComponent } from './features/users/user-details/user-details.component';
+import { CreateUserComponent } from './features/users/create-user/create-user.component';
+import { EditUserComponent } from './features/users/edit-user/edit-user.component';
 
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'users', component: UsersListComponent },
+    { path: 'users/create', component: CreateUserComponent },
+    { path: 'users/:id/edit', component: EditUserComponent },
     { path: 'users/:id', component: UserDetailsComponent },
+    
+
+    
     // wildcard for unknown routes
     { path: '**', redirectTo: 'dashboard' },
 ];
