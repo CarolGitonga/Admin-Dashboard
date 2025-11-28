9ea5fd92cefe1e630815b60f3ecc526cbc6aa1ae
CarolGitonga <carolgitongaofficial@gmail.com>
2025-11-28 15:00:43 +0300

    Add the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { UsersListComponent } from './features/users/users-list/users-list.component';
+import { UserDetailsComponent } from './features/users/user-details/user-details.component';
 
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'users', component: UsersListComponent },
+    { path: 'users/:id', component: UserDetailsComponent },
     // wildcard for unknown routes
     { path: '**', redirectTo: 'dashboard' },
 ];
